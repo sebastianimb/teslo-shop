9cc0c1604d7bdc944f93797fed10061aeaaae148
sebastian molina <cirp8918@gmail.com>
2026-04-09 12:34:51 -0400

    Add new product images for various items
    - Added multiple new images for products including 8528833, 8528839, 8528845, 8529100, 8529107, 8529198, 8529205, 8529212, 8529312, 8529318, 8529336, 8529342, 8529348, 8529354, 8529360, 8529366, 8529382, 8529387, 8764600, 8764613, 8764727, 8764734, 8764741, 8764754, 8764760, 8764766, 8764792, 8764806, 8764813, 8765085, 8765090, 8765095, 8765100, 8765105, 8765110, 8765115, 8765120, 8765125, 8765130, 9877034, and 9877040.
    - Created a new directory for uploads with a .gitkeep file to maintain the structure.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { FilesModule } from './files/files.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 //en prod synchronize sirve mas en false, ya que se deberian hacer migraciones.
 
@@ -20,9 +23,13 @@ import { SeedModule } from './seed/seed.module';
       autoLoadEntities: true,
       synchronize: true,
     }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
     ProductsModule,
     CommonModule,
     SeedModule,
+    FilesModule,
   ],
   controllers: [],
   providers: [],
